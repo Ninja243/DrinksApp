3a2244454c5ee7a4e8ef8630fce23dd779cb7f7f
Mweya Ruider <mweya@orcachat.com>
2021-02-17 16:34:00 +0100

    Slight reordering

---
+++ drinksapp/lib/screen/Home/view/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:drinksapp/screen/Ingredient/view/ingredient_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:drinksapp/screen/Ingredient/Ingredient.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<Home> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        bottomNavigationBar: BottomNavyBar(
+          selectedIndex: _currentIndex,
+          showElevation: true,
+          itemCornerRadius: 35,
+          curve: Curves.easeIn,
+          onItemSelected: (index) => setState(() => _currentIndex = index),
+          items: <BottomNavyBarItem>[
+            BottomNavyBarItem(
+                icon: Icon(Icons.food_bank_rounded),
+                title: Text('Ingredients')),
+            BottomNavyBarItem(
+                icon: Icon(Icons.local_drink_rounded), title: Text('Drinks')),
+            BottomNavyBarItem(
+                icon: Icon(Icons.settings), title: Text('Settings')),
+          ],
+        ),
+        body: getBody(_currentIndex));
+  }
+
+  Widget getBody(int index) {
+    switch (index) {
+      case 0:
+        return IngredientScreen();
+      case 1:
+        return Text('Drinks screen');
+      default:
+        return Text('Settings screen');
+    }
+  }
+}
