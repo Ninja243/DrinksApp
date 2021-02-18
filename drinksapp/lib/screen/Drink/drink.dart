55479c777852194adb796dd5bdf3a4ed389b9801
vinz <vinzock@gmail.com>
2021-02-18 17:17:27 +0100

    small changes

--- drinksapp/lib/screen/Drink/drink.dart
+++ drinksapp/lib/screen/Drink/drink.dart
@@ -1,4 +1,17 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
 class Drink extends StatefulWidget {
   @override
   _DrinkScreenState createState() => _DrinkScreenState();
+}
+
+class _DrinkScreenState extends State<Drink> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      listView: ListView(),
+    );
+  }
 }
